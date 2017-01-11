5443ab5ca36707161e1a80ad74f6a32e413362e2
Max <maximnorby@gmail.com>
2017-01-11 17:36:32 +0200

    added new folders with my projects

---
+++ practice/3_site/js/common.js
@@ -0,0 +1,82 @@
+$(document).ready(function() {
+
+	$(".top_menu_button").click(function() {
+		$(".top_menu ul").slideToggle();
+	});
+
+	$(".top_menu").find("a").click(function() {
+
+		if ($( window ).width() < 1024) {
+			$(".top_menu ul").slideToggle();
+		}
+	});
+
+	// Filter buttons
+	$(".portfolio_filter").on("click", ".tab", function(){
+		$(".portfolio_filter .tab").removeClass("active"); //удаляем класс во всех вкладках
+		$(this).addClass("active"); //добавляем класс текущей (нажатой)
+	});
+
+	//Навигация по Landing Page
+	//$(".top_mnu") - это верхняя панель со ссылками.
+	//Ссылки вида <a href="#contacts">Контакты</a>
+	//$(".top_menu").navigation();
+	$('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
+	//Добавляет классы дочерним блокам .block для анимации
+	//Документация: http://imakewebthings.com/jquery-waypoints/
+	$(".block").waypoint(function(direction) {
+		if (direction === "down") {
+			$(".class").addClass("active");
+		} else if (direction === "up") {
+			$(".class").removeClass("deactive");
+		};
+	}, {offset: 100});
+
+	//Плавный скролл до блока .div по клику на .scroll
+	//Документация: https://github.com/flesler/jquery.scrollTo
+	$("a.scroll").click(function() {
+		$.scrollTo($(".div"), 800, {
+			offset: -90
+		});
+	});
+
+	//Кнопка "Наверх"
+	//Документация:
+	//http://api.jquery.com/scrolltop/
+	//http://api.jquery.com/animate/
+	$("#top").click(function () {
+		$("body, html").animate({
+			scrollTop: 0
+		}, 800);
+		return false;
+	});
+	
+	//Аякс отправка форм
+	//Документация: http://api.jquery.com/jquery.ajax/
+	$("form").submit(function() {
+		$.ajax({
+			type: "GET",
+			url: "mail.php",
+			data: $("form").serialize()
+		}).done(function() {
+			alert("Спасибо за заявку!");
+			setTimeout(function() {
+				$.fancybox.close();
+			}, 1000);
+		});
+		return false;
+	});
+
+});
